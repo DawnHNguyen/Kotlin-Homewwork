c209a5522e129a6be1a7ecce2cb4901be05a82f1
YuuHaruna <55096320+YuuHaruna@users.noreply.github.com>
2019-10-02 23:17:49 +0700

    Add files via upload

--- Demo Kotlin.kt
+++ Demo Kotlin.kt
@@ -1,3 +1,9 @@
-fun main() {
+ fun main() {
     println("Em ko ga do em chua biet lam thoi")
+     printsum(16,4)
+
 }
+
+ fun printsum(a: Int, b: Int) {
+     println("sum of $a and $b is ${a+b}")
+ }
