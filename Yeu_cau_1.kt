24f4f4e6ca1c5c71d1cc5c4c8f65e5932042c217
YuuHaruna <55096320+YuuHaruna@users.noreply.github.com>
2019-10-10 23:57:48 +0700

    Add files via upload

---
+++ Yeu_cau_1.kt
@@ -0,0 +1,8 @@
+package com.k004.Lesson2
+ class Yeu_cau_1
+fun main(){
+    printname("Nguyễn Hoàng Đăng")
+}
+fun printname(name:String){
+    println(" Tên em là $name")
+}
---
+++ Yeu_cau_2.kt
@@ -0,0 +1,8 @@
+package com.k004.Lesson2
+
+fun main(){
+    printhinhthang(7,5,6)
+}
+ fun printhinhthang(a: Int,b: Int,h: Int ){
+   println("Diện tích hình thang có đáy lớn $a, đáy bé $b, chiều cao $h là ${(a+b)*h/2} ")
+ }
